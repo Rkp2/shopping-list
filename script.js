b1f90c561f9a1095178e7e2d689e5eeb36c09171
Rkp2 <pricemate1972@gmail.com>
2023-08-21 19:33:34 -0500

    added comments

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
 const itemForm = document.getElementById("item-form");
 const itemInput = document.getElementById("item-input");
 const itemList = document.getElementById("item-list");
+const clearBtn = document.getElementById("clear");
+const itemFilter = document.getElementById("filter");
 
 function addItem(e) {
   e.preventDefault();
 
   const newItem = itemInput.value;
 
-  // Validate Input
+  // if no input asks for one
   if (newItem === "") {
     alert("Please add an item");
     return;
   }
 
-  // Create list item
+  // Create li
   const li = document.createElement("li");
   li.appendChild(document.createTextNode(newItem));
-
+  // adds a li to the DOM
   const button = createButton("remove-item btn-link text-red");
   li.appendChild(button);
   itemList.appendChild(li);
+  checkUI();
   itemInput.value = "";
 }
 
@@ -36,6 +39,51 @@ function createIcon(classes) {
   icon.className = classes;
   return icon;
 }
+// targeting the red X to remove the li
+function removeItem(e) {
+  if (e.target.parentElement.classList.contains("remove-item")) {
+    if (confirm("Are you sure you want to remove?")) {
+      e.target.parentElement.parentElement.remove();
+      checkUI();
+    }
+  }
+}
+// clears the shopping list
+function clearItems() {
+  while (itemList.firstChild) {
+    itemList.removeChild(itemList.firstChild);
+  }
+  checkUI();
+}
+// filters the items in the list
+function filterItems(e) {
+  const items = itemList.querySelectorAll("li");
+  const text = e.target.value.toLowerCase();
+  items.forEach((item) => {
+    const itemName = item.firstChild.textContent.toLowerCase();
+    if (itemName.indexOf(text) != -1) {
+      item.style.display = "flex";
+    } else {
+      item.style.display = "none";
+    }
+  });
+}
+// shows clear all and filter items if li is populated else hides clear all filter items
+function checkUI() {
+  const items = itemList.querySelectorAll("li");
+  if (items.length === 0) {
+    clearBtn.style.display = "none";
+    itemFilter.style.display = "none";
+  } else {
+    clearBtn.style.display = "block";
+    itemFilter.style.display = "block";
+  }
+}
 
 // Event Listeners
 itemForm.addEventListener("submit", addItem);
+itemList.addEventListener("click", removeItem);
+clearBtn.addEventListener("click", clearItems);
+itemFilter.addEventListener("input", filterItems);
+
+checkUI();
